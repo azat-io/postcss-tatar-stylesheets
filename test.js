cf7f8da46171d9ce55ab331652ee595db59a6945
Azat S <to@azat.io>
2015-12-09 03:03:41 +0300

    Test update

--- test.js
+++ test.js
@@ -1,30 +1,16 @@
-var test = require('tape');
-var postcss = require('postcss');
-var plugin = require('./');
-var name = require('./package.json').name;
+import postcss from 'postcss';
+import test    from 'ava';
 
-var tests = [{
-    message: 'Should transform css',
-    fixture: 'div { фон: кызгылт-сары; калкавыч: уң; тышкыара: 30px 20px; өске-эчке-ара: 40px; блокның-дәрәҗәсе: 1 }',
-    expected: 'div { background: orange; float: right; margin: 30px 20px; padding-top: 40px; z-index: 1 }',
-    options: {foo: true}
-}];
+import plugin from './';
 
-function process (css, options) {
-    return postcss(plugin(options)).process(css).css;
+function run(t, input, output, opts = { }) {
+  return postcss([ plugin(opts) ]).process(input)
+    .then( result => {
+      t.same(result.css, output);
+      t.same(result.warnings().length, 0);
+    });
 }
 
-test(name, function (t) {
-    t.plan(tests.length);
-
-    tests.forEach(function (test) {
-        var options = test.options || {};
-        t.equal(process(test.fixture, options), test.expected, test.message);
-    });
-});
-
-test('should use the postcss plugin api', function (t) {
-    t.plan(2);
-    t.ok(plugin().postcssVersion, 'should be able to access version');
-    t.equal(plugin().postcssPlugin, name, 'should be able to access name');
+test('Do something', t => {
+  return run(t, 'div { фон: кызгылт-сары; калкавыч: уң; тышкыара: 30px 20px; өске-эчке-ара: 40px; блокның-дәрәҗәсе: 1 }', 'div { background: orange; float: right; margin: 30px 20px; padding-top: 40px; z-index: 1 }', { });
 });
